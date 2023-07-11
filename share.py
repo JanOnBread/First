88b4c8a8ee860b98f8e43ae3757c4d4ea4552fc0
Janet Cheung <jcworkandschool@gmail.com>
2023-07-11 18:41:45 +0100

    add lines

--- share.py
+++ share.py
@@ -1,6 +1,9 @@
 print("hellow oworld across the internet")
 print("the messgae above was written on my pc, this is a new message i wrote on my laptop")
-print ("This is my sister")
+print ("This is my sisrvvrvrter")
 print(" 2nd fork")
 print ("3rd fork or maybe a branch... idk this is stange ")
 print ("....")
+vvrvr
+wgmsldv,xc
+gwnesdvkmc, 
